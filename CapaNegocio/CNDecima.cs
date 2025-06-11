05e954f12f493984f860877c82211910f8564834
DOMENICA CAMILA PINTO ALVAREZ <domenica.pinto@epn.edu.ec>
2025-06-11 18:01:03 -0500

    Capa Negocio cambios

---
+++ CapaNegocio/CNDecima.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidades;
+
+namespace CapaNegocio
+{
+    public class CNDecima
+    {
+        public List<EDecima> listaDecimas = new List<EDecima>();
+
+
+    }
+}
