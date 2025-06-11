ac2f8e87abab04b9c957d845564a6000e17a167b
DOMENICA CAMILA PINTO ALVAREZ <domenica.pinto@epn.edu.ec>
2025-06-11 16:57:51 -0500

    CapaEntidades

---
+++ CapaEntidades/EDecima.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class EDecima
+    {
+        public int id { get; set; }
+        public DateTime fecha {  get; set; }
+        public string comentario { get; set; }
+
+        public double valor { get; set; }
+
+        
+        public EDecima() 
+        {
+            id = 0;
+            fecha = DateTime.Now;
+            comentario = "Sin comentario";
+            valor = 0.1;
+        }
+
+        // Constructor con parámetros
+        public EDecima(int id,DateTime fecha, string comentario, double valor)
+        {
+            this.id = id;
+            this.fecha = fecha;
+            this.comentario = comentario;
+            this.valor = valor;
+        }
+
+        // Método ToString para mostrar la información
+        public override string ToString()
+        {
+            return $"{fecha.ToShortDateString()} - {valor:F1} décimas - {comentario}";
+        }
+    }
+}
---
+++ CapaEntidades/EEstudiante.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class EEstudiante
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        private List<EDecima> decimasExtras { get; set; }
+
+        public EEstudiante()
+        {
+            decimasExtras = new List<EDecima>();
+        }
+
+        // Constructor con parámetros
+        public EEstudiante(int id, string nombre, string apellido)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.decimasExtras = new List<EDecima>();
+        }
+
+        // Método ToString para mostrar la información
+        public override string ToString()
+        {
+            return $"ID: {id} - Nombre: {nombre} - Décimas: {decimasExtras.Count}";
+        }
+    }
+}
