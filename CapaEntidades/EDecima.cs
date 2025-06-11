36225674d6a57f988575dcba412602425d803c0e
DOMENICA CAMILA PINTO ALVAREZ <domenica.pinto@epn.edu.ec>
2025-06-11 17:32:00 -0500

    m

--- CapaEntidades/EDecima.cs
+++ CapaEntidades/EDecima.cs
@@ -8,7 +8,7 @@ namespace CapaEntidades
 {
     public class EDecima
     {
-        public int Id { get; set; }
+        public int id { get; set; }
         public DateTime fecha {  get; set; }
         public string comentario { get; set; }
 
@@ -19,7 +19,7 @@ namespace CapaEntidades
         
         public EDecima() 
         {
-            Id = 0;
+            id = 0;
             fecha = DateTime.Now;
             comentario = "Sin comentario";
             valor = 0.1;
@@ -28,7 +28,7 @@ namespace CapaEntidades
         // Constructor con parámetros
         public EDecima(int id,DateTime fecha, string comentario, double valor)
         {
-            this.Id = id;
+            this.id = id;
             this.fecha = fecha;
             this.comentario = comentario;
             this.valor = valor;
